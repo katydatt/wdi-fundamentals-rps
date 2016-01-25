3b4a679a1be1e7a04e3f7d9d92972bec74360a60
Katy <katy.dattoma@gmail.com>
2016-01-26 10:23:22 +1100

    exercise solved with if statemants.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,25 +22,64 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
+    var move;
+    var x;
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    if (move == x) {
+    console.log(x);}
+    else (move == null) {
+    console.log('getInput()');
+    }
+    return 'getInput()'; 
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+  var move;
+  var x;
+  
+   if (move == x) {
+       console.log(x);}
+  else (move == null) {
+      console.log('randomPlay()');
+    } 
+    return 'randomPlay()'; 
 }
 
 function getWinner(playerMove,computerMove) {
     var winner;
+    var playerMove;
+    var computerMove;
+    
+    if (playerMove === computerMove) {
+    console.log("It's a tie!");
+    }
+    else if (playerMove === 'rock' && computerMove === 'scissors') {
+    console.log("Player wins!");
+    }
+    else if (playerMove === 'paper' && computerMove === 'rock') {
+    console.log("Player wins!");
+    }
+    else if (playerMove === 'scissors' && computerMove === 'paper') {
+    console.log("Player wins!");
+    }
+ else if (playerMove === 'scissors' && computerMove === 'rock') {
+    console.log("Computer wins!");
+    }
+     else if (playerMove === 'paper' && computerMove === 'scissors') {
+    console.log("Computer wins!");
+    }
+     else if (playerMove === 'rock' && computerMove === 'paper') {
+    console.log("Computer wins!");
+    }
+     
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
     return winner;
 }
 
